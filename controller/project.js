8c7b51008bfb4027f2198183838b7e07c9c6edf1
Hafiz <osaid@gmail.com>
2025-05-19 20:05:27 +0500

    final-code

---
+++ controller/project.js
@@ -0,0 +1,287 @@
+// controllers/HomeController.js
+const db = require('../config');  // Import the DB connection
+class ProjectController {
+     // Register a new user
+  async createproject(req, res) {
+    const { name, desc, owner } = req.body;
+
+    try {
+      // Register the new user
+      const [result] = await db.execute(
+        'INSERT INTO project (`project_name`, `project_description`, `project_owner`, `project_createdat`) VALUES (?, ?, ?,NOW())',
+        [name, desc, owner]
+      );
+
+      res.status(201).json({ msg: 'Project Created successfully', projectid: result.insertId });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+    }
+  }
+  
+  async team(req, res) {
+    const { id,developerIds } = req.body;
+
+  console.log("heyyy"+developerIds);
+
+    if (!developerIds || developerIds.length === 0) {
+      return res.status(400).json({ msg: 'Please provide project ID and selected developers' });
+    }
+  
+    try {
+      // Loop through the developerIds array and insert each developer only if not already assigned
+      
+        // Check if the developer is already assigned to the project
+        console.log(id,developerIds)
+        const [existingAssignment] = await db.execute(
+          'SELECT * FROM team WHERE project_id = ? AND developer_id = ?',
+          [id, developerIds]  // 1 is the static project ID
+        );
+  
+        // If the developer is already assigned, skip the insertion
+        if (existingAssignment.length > 0) {
+          console.log(`Developer ID ${developerIds} is already assigned to this project.`);
+        }
+  
+        // Insert the developer if not already assigned
+        await db.execute(
+          'INSERT INTO team (project_id, developer_id) VALUES (?, ?)',
+          [id, developerIds]  // 1 is the static project ID
+        );
+       
+       const [assigned] = await db.execute(
+        'select * from  team,users where project_id = ? and developer_id = userid',
+        [id]  // 1 is the static project ID
+      );
+
+      res.json({ msg: 'Team assigned successfully!' ,assigned_developers:assigned});
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: 'Error assigning team to project' });
+    }
+  }
+  async Assigned_Members(req, res) {
+    const id = req.params.project_id;
+    console.log(id);
+    try{
+      const [assigned] = await db.execute(
+        'select * from  team,users where project_id = ? and developer_id = userid',
+        [id]  // 1 is the static project ID
+      );
+  
+      res.json({ msg: 'Team Members' ,Team:assigned});
+    }catch(error)
+    {
+      res.status(500).json({ msg: 'Error assigning team to project' ,error});
+
+    }
+  
+  }
+
+  async UnAssigned_Team(req, res) {
+    const id = req.params.project_id;
+    const  developerIds  = req.body.developerIds;
+
+    console.log(developerIds);
+    try{
+      const [assigned] = await db.execute(
+        'Delete from team where project_id = ? and developer_id = ?',
+        [id,developerIds]  // 1 is the static project ID
+      );
+  
+      res.json({ msg: 'Team Member UnAssigned' ,Team:assigned});
+    }catch(error)
+    {
+      res.status(500).json({ msg: 'Error assigning team to project' ,error});
+
+    }
+  
+  }
+  async allproject(req, res) {
+    const { userrole, userid } = req.query;
+    console.log(userid,userrole);
+    try {
+      if(userrole == 3)
+        {
+          const [rows] = await db.execute(`SELECT DISTINCT project.* FROM project JOIN team ON project.project_id = team.project_id WHERE team.developer_id= ${userid}`);
+          res.json(rows);  // Send back an array of users
+        }
+        else if(userrole == 2)
+        {
+          const [rows] = await db.execute('SELECT * FROM project,team where project.project_id = team.project_id');
+          res.json(rows);  // Send back an array of users
+        }else
+        {
+          const [rows] = await db.execute('SELECT * FROM project');
+          res.json(rows);  // Send back an array of users
+
+        }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+    }
+  }
+  async addproject_interruptions(req,res)
+  {
+    const interruptId = Object.keys(req.body)[0];
+    const projectId = req.params.project_id; // Get`project_id` from URL parameter
+
+    try{
+      console.log(projectId,interruptId);
+      const [latest_estimation] = await db.execute(
+        'SELECT * FROM estimations WHERE project_id= ? order by estimation_id DESC limit 1',
+        [projectId]
+        );
+        if(latest_estimation)
+          {
+            const [existance_check] =  await db.execute(
+              'select * from project_interruptions where estimation_id = ? and interruption_id = ?',
+              [latest_estimation[0].estimation_id,interruptId]
+              );
+              if(existance_check.length <= 0)
+              {
+                await db.execute(
+                    'INSERT INTO project_interruptions(interruption_id,estimation_id) VALUES (?,?)',
+                    [interruptId,latest_estimation[0].estimation_id]
+                );
+                res.status(200).json({
+                  message: 'All Interruptions saved successfully...!'
+              });
+               }
+           }else{
+            res.status(500).json({ msg: 'Something is Happening..!' });
+
+           }
+    }catch(error)
+    {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+    }
+
+  }
+  async removeproject_interruptions(req,res)
+  {
+    const interruptId = Object.keys(req.body)[0];
+    const projectId = req.params.project_id; // Get `project_id` from URL parameter
+    
+    try{
+      const [latest_estimation] = await db.execute(
+        'SELECT * FROM estimations WHERE project_id= ? order by estimation_id DESC limit 1',
+        [projectId]
+        );
+        if(latest_estimation)
+          {
+                   const [existance_check] =  await db.execute(
+      'select * from project_interruptions where estimation_id = ? and interruption_id = ?',
+      [latest_estimation[0].estimation_id,interruptId]
+      );
+      if(existance_check.length <= 0)
+      {
+        await db.execute(
+            'delete from project_interruptions where estimation_id=? and interruption_id = ?',
+            [latest_estimation[0].estimation_id,interruptId]
+        );
+        res.status(200).json({
+          message: 'Interrupt Removed successfully...!'
+      });
+      }
+          }else{
+            res.status(500).json({ msg: 'Something is Happening..!' });
+
+           }
+
+    }catch(error)
+    {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+    }
+
+  }
+  async assigntask(req,res)
+  {
+    const developer_id = req.body.developer ;
+    const input_id = req.body.inputs;
+    const projectId = req.params.project_id; // Get`project_id` from URL parameter
+
+    try{
+      const [latest_estimation] = await db.execute(
+        'SELECT * FROM estimations WHERE project_id= ? order by estimation_id DESC limit 1',
+        [projectId]
+        );
+        var [verify_sprint] = await db.execute(
+          'select * from sprint where estimation_id = ?  order by sprint_id DESC limit 1',
+          [latest_estimation[0].estimation_id]
+      );
+        if(verify_sprint)
+          {
+            const [existance_check] =  await db.execute(
+              'select * from task where sprint_id = ? and input_id = ? and developer_id=?',
+              [verify_sprint[0].sprint_id,input_id,developer_id]
+              );
+              if(existance_check.length <= 0)
+              {
+                await db.execute(
+                    'INSERT INTO `task`(`sprint_id`, `input_id`, `developer_id`) VALUES (?,?,?)',
+                    [verify_sprint[0].sprint_id,input_id,developer_id]
+                );
+                res.status(200).json({
+                  message: 'All Task Assigned successfully...!'
+              });
+               }
+           }else{
+            res.status(500).json({ msg: 'Something is Happening..!' });
+
+           }
+    }catch(error)
+    {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+    }
+
+  }
+  async unassigntask(req,res)
+  {
+    const developer_id = req.body.developer ;
+    const input_id = req.body.inputs;
+    const projectId = req.params.project_id; // Get `project_id` from URL parameter
+    
+    try{
+      console.log(developer_id,input_id,projectId);
+      const [latest_estimation] = await db.execute(
+        'SELECT * FROM estimations WHERE project_id= ? order by estimation_id DESC limit 1',
+        [projectId]
+        );
+        var [verify_sprint] = await db.execute(
+          'select * from sprint where estimation_id = ?  order by sprint_id DESC limit 1',
+          [latest_estimation[0].estimation_id]
+      );
+        if(verify_sprint)
+          {
+            const [existance_check] =  await db.execute(
+              'select * from task where sprint_id = ? and input_id = ? and developer_id=?',
+              [verify_sprint[0].sprint_id,input_id,developer_id]
+              );
+              if(existance_check.length > 0)
+              {
+                await db.execute(
+                    'delete from task where sprint_id = ? and input_id = ? and developer_id=?',
+                    [verify_sprint[0].sprint_id,input_id,developer_id]
+                );
+                res.status(200).json({
+                  message: 'All Task Assigned successfully...!'
+              });
+               }
+           }else{
+            res.status(500).json({ msg: 'Something is Happening..!' });
+}
+    }catch(error)
+    {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+    }
+
+  }
+
+
+}
+module.exports = new ProjectController();
---
+++ controller/usecase.js
@@ -0,0 +1,119 @@
+const db = require('../config');  // Import the DB connection
+const usecasepoints = {
+    UC: {
+      Simple: 5,
+      Average: 10,
+      Complex: 15,
+    },
+  };
+  
+  function calculateuucp(inputCategory, complexity) {
+    // Check if the input category and complexity are valid
+    if (!usecasepoints[inputCategory] || !usecasepoints[inputCategory][complexity]) {
+      throw new Error('Invalid input category or complexity');
+    }
+  
+    // Return the function points for the given category and complexity
+    return usecasepoints[inputCategory][complexity];
+  }
+class usecase {
+
+    async uc_calculate(project,res,estimation_id) {
+        const  project_id = project;
+        const  estimationid = estimation_id;
+
+      
+        // If project_id is missing in the request body
+        // if (!project_id) {
+        //   return res.status(400).json({ error: 'Project ID is required' });
+        // }
+      
+        try {
+          // Query the MySQL database to get all input categories and complexities for the project
+          const [rows] = await db.execute(
+            'SELECT input_category, complexity FROM inputs WHERE project_id = ? AND and estimation_id = ? input_category NOT LIKE "%Question%" and input_category  LIKE "%UC%" ',
+            [project_id,estimationid]
+          );
+      
+          if (rows.length === 0) {
+            return null;
+          }
+      
+          // Initialize a variable to accumulate the total UFP
+          let totalUCP = 0;
+          let TCF = 0;
+          let ECF = 0;
+      
+          // Loop through each row and calculate the UFP
+          for (let i = 0; i < rows.length; i++) {
+            var { input_category, complexity } = rows[i];
+            var input_category = rows[i].input_category
+            // Validate the input category and complexity
+            if (!input_category || !complexity) {
+              console.error(`Invalid data at index ${i}: input_category or complexity is missing.`);
+              continue; // Skip invalid rows
+            }
+           
+           
+            else{
+                console.log(input_category,complexity);
+
+               // Calculate UFP for this row
+            var ucp = calculateuucp(input_category, complexity);
+            }
+            // Add the UFP for this input to the total UFP
+            totalUCP += ucp;
+          }
+          const [project_check] = await db.execute(
+            'SELECT * FROM estimations WHERE estimation_id = ? and estimation_method = "FC"',
+            [estimationid]
+          );
+          if(project_check.length > 0)
+          {
+            var [project_method] = await db.execute(
+              'SELECT sum(complexity) as ucp FROM inputs WHERE project_id = ? AND estimation_id = ? AND input_category NOT LIKE "%Question%"',
+              [project_id,estimationid]
+            );
+            totalUCP += project_method[0]["ucp"];
+
+          }
+          const [project_method2] = await db.execute(
+            'SELECT sum(complexity) as tcf FROM inputs WHERE project_id = ? AND estimation_id = ?  and input_category like "%Question%"  and input_category LIKE "%TCF%"',
+            [project_id,estimationid]
+          );
+          const [project_method3] = await db.execute(
+            'SELECT sum(complexity) as ecf FROM inputs WHERE project_id = ? AND estimation_id = ?  and input_category like "%Question%"  and input_category LIKE "%ECF%"',
+            [project_id,estimationid]
+          );
+        
+
+           TCF += project_method2[0]["tcf"];
+           ECF += project_method3[0]["ecf"];
+           var Final_TCF = 0.6 + (TCF / 100);
+           var Final_ECF = 1.4 + (-0.03 * ECF);
+
+           var UC = totalUCP * Final_ECF * Final_TCF;
+           const [avg_effort] = await db.execute('SELECT AVG(Effort_person_month) as avge FROM `fp_historical_data`');
+           const [team_members] = await db.execute('SELECT count(team_id) as tt FROM team where project_id = ?',[project_id]);
+        //    const [avg_cost] = await db.execute('SELECT AVG(cost) as avgc FROM `fp_historical_data`');
+        //    const [avg_fp] = await db.execute('SELECT AVG(FP) as avgf FROM `fp_historical_data`');
+
+           console.log(avg_effort,team_members);
+           var Effort = 20 * UC;
+           var Time = 20 / team_members[0]["tt"];
+           var cost_per_fp = 45000 / UC;
+           var cost = cost_per_fp * UC;
+           if(UC)
+           {
+            const [save_ouput] = await db.execute('INSERT INTO `project_estimation`(`project_id`, `estimation_technique_id`, `effort`, `time`, `cost`) VALUES (?,?,?,?,?)',
+              [project_id,estimationid,Effort,Time,cost]);
+           }
+          // Send the total UFP in the response
+      
+        } catch (error) {
+          console.error('Database error:', error);
+        }
+      }
+
+}
+module.exports = new usecase();
