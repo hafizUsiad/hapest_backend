edbe70232149b54b4cec9746ff44bee1c94044fc
Hafiz Muhammad OSAID Ahmed <149686768+hafizUsiad@users.noreply.github.com>
2025-05-20 11:47:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 // Create a MySQL connection pool
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
 // const connection = mysql.createConnection({
 //   host: 'fyp.mysql.database.azure.com', // Azure MySQL host
@@ -18,7 +18,14 @@ const pool = mysql.createPool({
 //   port: 3306, // MySQL port
 //   connectTimeout: 10000, // Increase timeout if needed
 // });
-
+const pool = mysql.createPool({
+  host: 'hapest-hafiz-ab2a.g.aivencloud.com',
+  port: 25524,
+  user: 'avnadmin',
+  password: 'AVNS_m0tSO8YhYx3D3c92pch',
+  database: 'fyp',
+  connectTimeout: 100000,
+});
 const db = pool.promise();  // Using promise-based queries
 
 module.exports = db;
